f265695f3e62ebecafbcd0ab979f718fc39d6fff
rpolanco <rpolanco@migracion.gob.do>
2020-08-14 22:25:04 -0400

    Add project files.

---
+++ Plinq1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Plinq1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sourceRange = Enumerable.Range(1, 10000);
+            var evenNums = from num in sourceRange.AsParallel()
+                           where num % 2 == 0
+                           select num;
+            Console.WriteLine($"{evenNums.Count()} even numbers out of  {sourceRange.Count()} total");
+        }
+    }
+}
